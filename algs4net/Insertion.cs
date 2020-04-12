d1d1155c29186cf3f8f65eb6da3929def6259fd7
robertrancz <rancz.robi@gmail.com>
2020-04-12 17:27:21 +0300

    Add insertion and merge sort

---
+++ algs4net/Insertion.cs
@@ -0,0 +1,186 @@
+﻿/******************************************************************************
+ *  File name :    Insertion.cs
+ *  Demo test :    Use the algscmd util or Visual Studio IDE
+ *            :    Enter algscmd alone for how to use the util
+ *  Data files:   http://algs4.cs.princeton.edu/21sort/tiny.txt
+ *                http://algs4.cs.princeton.edu/21sort/words3.txt
+ *
+ *  Sorts a sequence of strings from standard input using insertion sort.
+ *
+ *  C:\> type tiny.txt
+ *  S O R T E X A M P L E
+ *
+ *  C:\> algscmd Insertion < tiny.txt
+ *  A E E L M O P R S T X                 [ one string per line ]
+ *
+ *  C:\> type words3.txt
+ *  bed bug dad yes zoo ... all bad yet
+ *
+ *  C:\> algscmd Insertion < words3.txt
+ *  all bad bed bug dad ... yes yet zoo   [ one string per line ]
+ *
+ ******************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+
+namespace algs4net
+{
+    /// <summary><para>
+    /// The <c>Insertion</c> class provides static methods for sorting an array using insertion sort.
+    /// </para><para>
+    /// This implementation makes ~ 1/2 N^2 compares and OrderHelper.Exchanges in
+    /// the worst case, so it is not suitable for sorting large arbitrary arrays!
+    /// More precisely, the number of OrderHelper.Exchanges is exactly equal to the number
+    /// of inversions. So, for example, it sorts a partially-sorted array in linear time.
+    /// </para><para>
+    /// The sorting algorithm is stable and uses O(1) extra memory.
+    /// </para>
+    /// <para>
+    /// See <a href="http://algs4.cs.princeton.edu/21elementary/InsertionPedantic.java.html">InsertionPedantic.java</a>
+    /// for a version that eliminates the compiler warning.
+    /// </para></summary>
+    /// <remarks>
+    /// For additional documentation, see <a href="http://algs4.cs.princeton.edu/21elementary">Section 2.1</a> of
+    /// <em>Algorithms, 4th Edition</em> by Robert Sedgewick and Kevin Wayne.
+    /// <para>This class is a C# port from the original Java class 
+    /// <a href="http://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/Insertion.java.html">Insertion</a> implementation by
+    /// Robert Sedgewick and Kevin Wayne.
+    /// </para></remarks>
+    ///
+    public static class Insertion
+    {
+
+        // This class should not be instantiated.
+        static Insertion() { }
+
+        /// <summary>
+        /// Rearranges the array in ascending order, using the natural order.</summary>
+        /// <param name="a">a the array to be sorted</param>
+        ///
+        public static void Sort(IComparable[] a)
+        {
+            if(a == null)
+                throw new ArgumentNullException(nameof(a), "The array to be sorted cannot be null");
+            
+            int N = a.Length;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i; j > 0 && OrderHelper.Less(a[j], a[j - 1]); j--)
+                {
+                    OrderHelper.Exch(a, j, j - 1);
+                }
+                Debug.Assert(OrderHelper.IsSorted(a, 0, i));
+            }
+            Debug.Assert(OrderHelper.IsSorted(a));
+        }
+
+        /// <summary>
+        /// Rearranges the subarray a[lo..hi] in ascending order, using the natural order.</summary>
+        /// <param name="a">a the array to be sorted</param>
+        /// <param name="lo">lo left endpoint</param>
+        /// <param name="hi">hi right endpoint</param>
+        ///
+        public static void Sort(IComparable[] a, int lo, int hi)
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a), "The array to be sorted cannot be null");
+
+            for (int i = lo; i <= hi; i++)
+            {
+                for (int j = i; j > lo && OrderHelper.Less(a[j], a[j - 1]); j--)
+                {
+                    OrderHelper.Exch(a, j, j - 1);
+                }
+            }
+            Debug.Assert(OrderHelper.IsSorted(a, lo, hi));
+        }
+
+        /// <summary>
+        /// Rearranges the array in ascending order, using a comparator.</summary>
+        /// <param name="a">a the array</param>
+        /// <param name="comparator">comparator the comparator specifying the order</param>
+        ///
+        public static void Sort(object[] a, System.Collections.Comparer comparator)
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a), "The array to be sorted cannot be null");
+
+            int N = a.Length;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i; j > 0 && OrderHelper.Less(a[j], a[j - 1], comparator); j--)
+                {
+                    OrderHelper.Exch(a, j, j - 1);
+                }
+                Debug.Assert(OrderHelper.IsSorted(a, 0, i, comparator));
+            }
+            Debug.Assert(OrderHelper.IsSorted(a, comparator));
+        }
+
+        /// <summary>
+        /// Rearranges the subarray a[lo..hi] in ascending order, using a generic comparator.</summary>
+        /// <param name="a">a the array</param>
+        /// <param name="lo">lo left endpoint</param>
+        /// <param name="hi">hi right endpoint</param>
+        /// <param name="comparator">comparator the comparator specifying the order</param>
+        ///
+        /// <exception cref="ArgumentNullException"><paramref name="comparator"/> is <c>null</c>.</exception>
+        public static void Sort<T>(T[] a, int lo, int hi, Comparer<T> comparator)
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a), "The array to be sorted cannot be null");
+            if (comparator == null)
+                throw new ArgumentNullException(nameof(comparator), "The comparator cannot be null");
+
+            for (int i = lo; i <= hi; i++)
+            {
+                for (int j = i; j > lo && OrderHelper.Less(a[j], a[j - 1], comparator); j--)
+                {
+                    OrderHelper.Exch<T>(a, j, j - 1);
+                }
+            }
+            Debug.Assert(OrderHelper.IsSorted(a, lo, hi, comparator));
+        }
+
+        /// <summary>
+        /// Returns a permutation that gives the elements in the array in ascending order,
+        /// while not changing the original array a[]</summary>
+        /// <param name="a">a the array</param>
+        /// <returns>a permutation <c>p[]</c> such that <c>a[p[0]]</c>, <c>a[p[1]]</c>,
+        /// ..., <c>a[p[N-1]]</c> are in ascending order</returns>
+        ///
+        public static int[] IndexSort(int[] a)
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a), "The array to be sorted cannot be null");
+
+            int N = a.Length;
+            int[] index = new int[N];
+            for (int i = 0; i < N; i++)
+                index[i] = i;
+
+            for (int i = 0; i < N; i++)
+                for (int j = i; j > 0 && OrderHelper.Less(a[index[j]], a[index[j - 1]]); j--)
+                    OrderHelper.Exch(index, j, j - 1);
+
+            return index;
+        }
+
+        /// <summary>
+        /// Reads in a sequence of strings from standard input; insertion sorts them;
+        /// and prints them to standard output in ascending order.</summary>
+        /// <param name="args">Place holder for user arguments</param>
+        ///
+        [HelpText("algscmd Insertion < words3.txt", "Input strings to be printed in sorted order")]
+        public static void MainTest(string[] args)
+        {
+            TextInput StdIn = new TextInput();
+            string[] a = StdIn.ReadAllStrings();
+            Insertion.Sort(a);
+            OrderHelper.Show(a);
+        }
+    }
+}
---
+++ algs4net/MergeX.cs
@@ -0,0 +1,181 @@
+﻿/******************************************************************************
+ *  File name :    MergeX.cs
+ *  Demo test :    Use the algscmd util or Visual Studio IDE
+ *            :    Enter algscmd alone for how to use the util
+ *  Data files:   http://algs4.cs.princeton.edu/22mergesort/tiny.txt
+ *                http://algs4.cs.princeton.edu/22mergesort/words3.txt
+ *   
+ *  Sorts a sequence of strings from standard input using an
+ *  optimized version of mergesort.
+ *   
+ *  C:\> type tiny.txt
+ *  S O R T E X A M P L E
+ *
+ *  C:\> algscmd MergeX < tiny.txt
+ *  A E E L M O P R S T X                 [ one string per line ]
+ *    
+ *  C:\> type words3.txt
+ *  bed bug dad yes zoo ... all bad yet
+ *  
+ *  C:\> algscmd MergeX < words3.txt
+ *  all bad bed bug dad ... yes yet zoo    [ one string per line ]
+ *
+ ******************************************************************************/
+
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algs4net
+{
+    /// <summary>
+    /// The <c>MergeX</c> class provides static methods for sorting an array using an optimized version of mergesort.</summary>
+    /// <remarks><para>
+    /// For additional documentation, see <a href="http://algs4.cs.princeton.edu/22mergesort">Section 2.2</a> of
+    /// <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.</para>
+    /// <para>This class is a C# port from the original Java class 
+    /// <a href="http://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/MergeX.java.html">MergeX</a>
+    /// implementation by the respective authors.</para></remarks>
+    ///
+    public static class MergeX
+    {
+        private const int CUTOFF = 7;  // cutoff to insertion sort
+
+        // This class should not be instantiated.
+        static MergeX() { }
+
+        private static void Merge(IComparable[] src, IComparable[] dst, int lo, int mid, int hi)
+        {
+            // precondition: src[lo .. mid] and src[mid+1 .. hi] are sorted subarrays
+            Debug.Assert(OrderHelper.IsSorted(src, lo, mid));
+            Debug.Assert(OrderHelper.IsSorted(src, mid + 1, hi));
+
+            int i = lo, j = mid + 1;
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid) dst[k] = src[j++];
+                else if (j > hi) dst[k] = src[i++];
+                else if (OrderHelper.Less(src[j], src[i])) dst[k] = src[j++];   // to ensure stability
+                else dst[k] = src[i++];
+            }
+
+            // postcondition: dst[lo .. hi] is sorted subarray
+            Debug.Assert(OrderHelper.IsSorted(dst, lo, hi));
+        }
+
+        private static void Sort(IComparable[] src, IComparable[] dst, int lo, int hi)
+        {
+            // if (hi <= lo) return;
+            if (hi <= lo + CUTOFF)
+            {
+                Insertion.Sort(dst, lo, hi);
+                return;
+            }
+            int mid = lo + (hi - lo) / 2;
+            Sort(dst, src, lo, mid);
+            Sort(dst, src, mid + 1, hi);
+
+            if (!OrderHelper.Less(src[mid + 1], src[mid]))
+            {
+                Array.Copy(src, lo, dst, lo, hi - lo + 1);
+                return;
+            }
+            Merge(src, dst, lo, mid, hi);
+        }
+
+        /// <summary>
+        /// Rearranges the array in ascending order, using the natural order.</summary>
+        /// <param name="a">the array to be sorted</param>
+        ///
+        public static void Sort(IComparable[] a)
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a), "The array to be sorted cannot be null");
+
+            IComparable[] aux = new IComparable[a.Length];
+            a.CopyTo(aux, 0);
+            Sort(aux, a, 0, a.Length - 1);
+            Debug.Assert(OrderHelper.IsSorted(a));
+        }
+
+        /*******************************************************************
+         *  Version that takes Comparator as argument.
+         *******************************************************************/
+
+        /// <summary>
+        /// Rearranges the array in ascending order, using the provided order.</summary>
+        /// <param name="a">the array to be sorted</param>
+        /// <param name="comparator">the user comparator</param>
+        ///
+        public static void Sort<T>(T[] a, Comparer<T> comparator)
+        {
+            if (a == null)
+                throw new ArgumentNullException(nameof(a), "The array to be sorted cannot be null");
+
+            T[] aux = new T[a.Length];
+            a.CopyTo(aux, 0);
+            Sort(aux, a, 0, a.Length - 1, comparator);
+            Debug.Assert(OrderHelper.IsSorted(a, comparator));
+        }
+
+        private static void Merge<T>(T[] src, T[] dst, int lo, int mid, int hi, Comparer<T> comparator)
+        {
+            // precondition: src[lo .. mid] and src[mid+1 .. hi] are sorted subarrays
+            Debug.Assert(OrderHelper.IsSorted(src, lo, mid, comparator));
+            Debug.Assert(OrderHelper.IsSorted(src, mid + 1, hi, comparator));
+
+            int i = lo, j = mid + 1;
+            for (int k = lo; k <= hi; k++)
+            {
+                if (i > mid) dst[k] = src[j++];
+                else if (j > hi) dst[k] = src[i++];
+                else if (OrderHelper.Less(src[j], src[i], comparator)) dst[k] = src[j++];
+                else dst[k] = src[i++];
+            }
+
+            // postcondition: dst[lo .. hi] is sorted subarray
+            Debug.Assert(OrderHelper.IsSorted(dst, lo, hi, comparator));
+        }
+
+
+        private static void Sort<T>(T[] src, T[] dst, int lo, int hi, Comparer<T> comparator)
+        {
+            // if (hi <= lo) return;
+            if (hi <= lo + CUTOFF)
+            {
+                Insertion.Sort(dst, lo, hi, comparator);
+                return;
+            }
+            int mid = lo + (hi - lo) / 2;
+            Sort(dst, src, lo, mid, comparator);
+            Sort(dst, src, mid + 1, hi, comparator);
+
+            if (!OrderHelper.Less(src[mid + 1], src[mid], comparator))
+            {
+                Array.Copy(src, lo, dst, lo, hi - lo + 1);
+                return;
+            }
+
+            Merge(src, dst, lo, mid, hi, comparator);
+        }
+
+        /// <summary>
+        /// Reads in a sequence of strings from standard input; mergesorts them
+        /// (using an optimized version of mergesort); 
+        /// and prints them to standard output in ascending order. 
+        /// </summary>
+        /// <param name="args">Place holder for user arguments</param>
+        /// 
+        [HelpText("algscmd MergeX < words3.txt", "Input strings to be printed in sorted order")]
+        public static void MainTest(string[] args)
+        {
+            TextInput StdIn = new TextInput();
+            string[] a = StdIn.ReadAllStrings();
+            MergeX.Sort(a);
+            OrderHelper.Show(a);
+        }
+    }
+}
