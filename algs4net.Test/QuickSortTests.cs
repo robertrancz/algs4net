d6a4f89d877b841eb47d28f0bd5972cdb4060e00
robertrancz <rancz.robi@gmail.com>
2020-04-27 23:37:37 +0300

    Add undirected graph data structure and tests.

---
+++ algs4net.Test/QuickSortTests.cs
@@ -0,0 +1,31 @@
+﻿using algs4net;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algs4NetUnitTests
+{
+    [TestClass]
+    public class QuickTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void QuickTest1()
+        {
+            string[] a = new string[] { "aba" };
+            string s;
+
+            Quick.Sort(a);
+            s = (string)Quick.Select(a, 0);
+            Assert.AreEqual(s, a[0]);
+
+            a = new string[] { "zoo", "able", "after", "cury", "aba", "bed", "bug", "boy", "bing", " " };
+            s = (string)Quick.Select(a, a.Length - 1);
+            Assert.AreEqual(s, "zoo");
+
+            Quick.Sort(a);
+            Assert.AreEqual("aba", a[1]);
+
+            Quick.Select(a, a.Length); // generate exception
+        }
+    }
+}
